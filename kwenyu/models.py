c5dc40b86d460a6d23db907abc7a07e2c2c9b8bf
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-11-05 13:10:05 +0300

    Model business

--- kwenyu/models.py
+++ kwenyu/models.py
@@ -18,9 +18,7 @@ class Neighbourhood (models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     picture = CloudinaryField('image')
-    admin = models.ForeignKey(Profile, on_delete=models.CASCADE)    
-    health = models.IntegerField(null=True, blank=True)
-    police = models.IntegerField(null=True, blank=True)
+    admin = models.ForeignKey(Profile, on_delete=models.CASCADE,related_name='admin')    
 
     def __str__(self):
         return self.name
@@ -30,3 +28,14 @@ class Neighbourhood (models.Model):
 
     def delete_neighborhood(self):
         self.delete()
+
+    class Business(models.Model):
+        name = models.CharField(max_length=120)
+        email = models.EmailField(max_length=254)
+        description = models.TextField(blank=True)
+        neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, related_name='business')
+        user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+
+        def __str__(self):
+            return self.name
+
